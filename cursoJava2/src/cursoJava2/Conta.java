fe14d7a54241f1afcb441fcc0492c487d3b42457
Walter <wsimoes82@gmail.com>
2022-02-13 17:58:23 -0300

    Iniciado novo Curso do Alura de Java

---
+++ cursoJava2/src/cursoJava2/Conta.java
@@ -0,0 +1,16 @@
+package cursoJava2;
+
+public class Conta {
+		//Abaixo segue os atributos da conta bancaria
+		// pode se tratar como uma especificação para a classe Conta
+		
+		//Objeto ou instancia é criado a partir da classe Conta, ou seja,
+		// a partir das especificações da Classe Conta
+	
+	
+		double saldo;
+		int agencia;
+		int numero;
+		String titular;
+
+}
---
+++ cursoJava2/src/cursoJava2/CriaConta.java
@@ -0,0 +1,20 @@
+package cursoJava2;
+
+public class CriaConta {
+	public static void main(String[] args) {
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 200;
+		System.out.println(primeiraConta.saldo);
+		
+		//soma 100 ao valor que ele tinha na variavel
+		primeiraConta.saldo += 100;
+		
+		System.out.println(primeiraConta.saldo);
+		
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 50;
+		System.out.println(segundaConta.saldo);
+		
+	}
+
+}
