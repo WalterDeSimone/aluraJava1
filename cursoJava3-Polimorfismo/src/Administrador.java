e0953125b9814c330ed76b01a51026e21ffbba50
Walter <wsimoes82@gmail.com>
2022-02-27 00:55:47 -0300

    Polimorfismo: Entenda herança e interfaces

--- cursoJava3-Polimorfismo/src/Administrador.java
+++ cursoJava3-Polimorfismo/src/Administrador.java
@@ -1,7 +1,7 @@
 
 public class Administrador extends Funcionario implements Autenticavel {
 
-	private int senha;
+	//private int senha;
 	
 	@Override
 	public double getBonificacao() {
@@ -9,18 +9,21 @@ public class Administrador extends Funcionario implements Autenticavel {
 		return 50;
 	}
 
+	private AutenticacaoUtil autenticador;
+	
+	public Administrador() {
+		this.autenticador = new AutenticacaoUtil();
+	}
+	
+	
 	@Override
 	public void setSenha(int senha) {
-		this.senha = senha;	
+		this.autenticador.setSenha(senha);		
 	}
 
 	@Override
 	public boolean autentica(int senha) {
-		if(this.senha == senha) {
-			return true;
-		} else {
-			return false;	
-		}
+		return this.autenticador.autentica(senha);
 	}
 
 }
--- cursoJava3-Polimorfismo/src/Gerente.java
+++ cursoJava3-Polimorfismo/src/Gerente.java
@@ -19,25 +19,26 @@ public class Gerente extends Funcionario implements Autenticavel{
 		}
 	}
 	*/
-	private int senha;
 	
 	public double getBonificacao () {
 		return super.getSalario(); //super. indica que vem da classe mae, que nao esta definido nesta classe, retirado o this.
 	}
 
+	private AutenticacaoUtil autenticador;
+	
+	public Gerente() {
+		this.autenticador = new AutenticacaoUtil();
+	}
+	
+	
 	@Override
 	public void setSenha(int senha) {
-		this.senha = senha;	
+		this.autenticador.setSenha(senha);		
 	}
 
 	@Override
 	public boolean autentica(int senha) {
-		if(this.senha == senha) {
-			return true;
-		} else {
-			return false;	
-		}
+		return this.autenticador.autentica(senha);
 	}
-
 
 }
