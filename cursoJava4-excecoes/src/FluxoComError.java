5c60d81aa0ce6dfd4803ec43af891e7cd036319e
Walter <wsimoes82@gmail.com>
2022-03-04 01:42:47 -0300

    Checked e Unchecked

--- cursoJava4-excecoes/src/FluxoComError.java
+++ cursoJava4-excecoes/src/FluxoComError.java
@@ -1,5 +1,5 @@
 
-public class Fluxo {
+public class FluxoComError {
 	public static void main(String[] args) {
         System.out.println("Ini do main");
         
@@ -14,7 +14,7 @@ public class Fluxo {
         System.out.println("Fim do main");
     }
 
-    private static void metodo1() throws MinhaExcecao {
+    private static void metodo1() {
         System.out.println("Ini do metodo1");
 
         metodo2();
@@ -22,14 +22,9 @@ public class Fluxo {
         System.out.println("Fim do metodo1");
     }
 
-    private static void metodo2() throws MinhaExcecao {
-        System.out.println("Ini do metodo2");
-        
-        //ArithmeticException ex = new ArithmeticException("deu errado");
-        //throw ex; OU:
-        
-        throw new MinhaExcecao("deu errado");
-        
-        //System.out.println("Fim do metodo2");
+    private static void metodo2() {
+      System.out.println("Ini do metodo2");
+    	metodo2();
+    	System.out.println("Fim do metodo2");
     }
 }
